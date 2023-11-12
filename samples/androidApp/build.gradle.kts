dd9685d2a812bc0a128c044c896cae2915dc9e92
fankesyooni <qzmmcn@163.com>
2023-11-13 04:22:02 +0800

    chore: use debug signing release

--- samples/androidApp/build.gradle.kts
+++ samples/androidApp/build.gradle.kts
@@ -34,6 +34,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
