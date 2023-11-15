8e09ae9a1abdbb534e77ba2823ab82246f1ec9c5
fankesyooni <qzmmcn@163.com>
2023-11-16 02:42:19 +0800

    feat: add Popup

--- flexiui-core/src/commonMain/kotlin/com/highcapable/flexiui/component/Popup.kt
+++ flexiui-core/src/commonMain/kotlin/com/highcapable/flexiui/component/Popup.kt
@@ -17,10 +17,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * This file is created by fankes on 2023/11/9.
+ * This file is created by fankes on 2023/11/5.
  */
 @file:Suppress("unused")
 
 package com.highcapable.flexiui.component
 
-// TODO: To be implemented
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.window.PopupPositionProvider
+import androidx.compose.ui.window.PopupProperties
+
+@Composable
+expect fun Popup(
+    alignment: Alignment = Alignment.TopStart,
+    offset: IntOffset = IntOffset(0, 0),
+    popupPositionProvider: PopupPositionProvider? = null,
+    onDismissRequest: (() -> Unit)? = null,
+    properties: PopupProperties = PopupProperties(),
+    onPreviewKeyEvent: ((KeyEvent) -> Boolean)? = null,
+    onKeyEvent: ((KeyEvent) -> Boolean)? = null,
+    content: @Composable () -> Unit
+)
