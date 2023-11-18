37566af60386b66d796897c3cbba0aa197090157
fankesyooni <qzmmcn@163.com>
2023-11-18 18:46:35 +0800

    style: optimize code

--- flexiui-core/src/iosMain/kotlin/com/highcapable/flexiui/Colors.ios.kt
+++ flexiui-core/src/iosMain/kotlin/com/highcapable/flexiui/Colors.ios.kt
@@ -26,12 +26,12 @@ package com.highcapable.flexiui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 
+@Composable
+@ReadOnlyComposable
+actual fun isDynamicColorsAvailable() = false
+
 internal actual val DynamicLightColors = DefaultLightColors
 
 internal actual val DynamicDarkColors = DefaultDarkColors
 
 internal actual val DynamicBlackColors = DefaultBlackColors
-
-@Composable
-@ReadOnlyComposable
-actual fun isDynamicColorsAvailable() = false
